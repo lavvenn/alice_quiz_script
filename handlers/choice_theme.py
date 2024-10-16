7e5549a6651cec8c43f107077a9de332ae5e5d86
lavvenn <zmievsijkonstantin0@gamil.com>
2024-10-16 20:43:56 +0300

    addthe ability to exit the quiz

--- handlers/choice_theme.py
+++ handlers/choice_theme.py
@@ -6,9 +6,9 @@ from quizer.quizes import geo_quiz, math_quiz, quiz_list
 
 
 def choice_theme(request,response, all_quizzes: dict):
-    if request['request']['command'] in quiz_list:
+    if request['request']['original_utterance'] in quiz_list:
 
-        all_quizzes[request['session']['user_id']] = copy.deepcopy(quiz_list[request['request']['command']])
+        all_quizzes[request['session']['user_id']] = copy.deepcopy(quiz_list[request['request']['original_utterance']])
 
         response['response']['text'] = all_quizzes[request['session']['user_id']].print_current_question()
         response['session_state'] = {'state': 'in_quiz'}
--- handlers/in_quiz.py
+++ handlers/in_quiz.py
@@ -1,2 +1,7 @@
 def in_quiz(request, response, all_quizzes: dict):
-    response['response']['text'] = all_quizzes[request['session']['user_id']].next_question(request['request']['command'])
+    try:
+        response['response']['text'] = all_quizzes[request['session']['user_id']].next_question(request['request']['command'])
+
+    except IndexError:
+        response['response']['text'] = 'вы прошли все вопросы. если хотите начать заново, скажите "темы"'
+        response['session_state'] = {'state': 'start'}
